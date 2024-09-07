f1de7e4ee299d1dc09c380f7ae550d4d8f8b4d36
hamis1804 <duthuha1804@gmail.com>
2024-09-07 09:37:40 +0700

    Topic_01_Check_Environment

---
+++ selenium/webdriver/Topic_00_Template.java
@@ -0,0 +1,45 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_00_Template {
+    WebDriver driver;
+
+
+    @BeforeClass
+    public void initialBrowser() {
+        driver = new FirefoxDriver();
+        driver.get("https://demo.nopcommerce.com/");
+
+    }
+
+    @Test
+    public void TC_01_() {
+
+    }
+
+    @Test
+    public void TC_02_() {
+
+    }
+
+    @Test
+    public void TC_03_() {
+
+    }
+
+
+
+    @AfterClass
+
+    public void cleanBrowser() {
+        driver.quit();
+
+    }
+}
